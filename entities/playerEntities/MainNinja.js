3c113213f4bc1615dc36455705556826fde64c57
Nhat499 <nhat499@uw.edu>
2022-01-26 16:19:45 -0800

    testing webhook

--- entities/playerEntities/MainNinja.js
+++ entities/playerEntities/MainNinja.js
@@ -284,6 +284,7 @@ class MainNinja {
                             //self.updateBB();
                         }
                     }  
+                    
                 //}
 
                 
