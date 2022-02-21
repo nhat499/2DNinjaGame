2d34eeb8a51e702b2cbf560f06ef9bb5076f84d8
Arman <arman@nytesoftware.com>
2022-02-20 16:40:31 -0800

    Add game over screen when ninja dies

--- SceneManager.js
+++ SceneManager.js
@@ -1,133 +1,145 @@
 class SceneManager {
-    constructor(game) {
-        this.game = game;
-        this.game.camera = this;
-        this.x = 0 //horizontal scrolling
-        this.y = 0
-        //this.health = 0;
-        // this.lives = 0; 
-        this.gameOver = false;
-        this.title = true;
-        this.level = level1;
+  constructor(game) {
+    this.game = game;
+    this.game.camera = this;
+    this.x = 0; //horizontal scrolling
+    this.y = 0;
+    //this.health = 0;
+    // this.lives = 0;
+    this.gameOver = false;
+    this.title = true;
+    this.level = level1;
 
-        this.sound = new Audio();
-        this.sound.src = "./music/edson1.wav";
+    this.sound = new Audio();
+    this.sound.src = './music/edson1.wav';
 
+    //this.loadLevel1();
+    //console.log(level1);
+    //this.loadLevel(this.level);
 
-        //this.loadLevel1();
-        //console.log(level1);
-        //this.loadLevel(this.level);
+    //let knight = new Knight(this.game, 600, 100);
+    //this.game.addEntity(knight);
+    //let slime = new Slime(this.game, 1000, -100);
+    //this.game.addEntity(slime);
 
-        //let knight = new Knight(this.game, 600, 100);
-        //this.game.addEntity(knight);
-        //let slime = new Slime(this.game, 1000, -100);
-        //this.game.addEntity(slime);
-        
-        //this.game.addEntity(this.knight);
-        //this.game.addEntity(this.ghost);
-        this.ninja = new MainNinja(this.game, 0,170);
-        this.game.addEntity(this.ninja);
+    //this.game.addEntity(this.knight);
+    //this.game.addEntity(this.ghost);
+    this.ninja = new MainNinja(this.game, 0, 170);
+    this.game.addEntity(this.ninja);
+  }
 
+  update() {
+    if (this.title) {
+      this.sound.play();
     }
 
-    update() {
-
-        if (this.title) {
-            this.sound.play();
-        }
-
-        // update horizontal camera
-        let leftPoint = this.game.surfaceWidth / 3;
-        let rightPoint = this.game.surfaceWidth - leftPoint;
-        if ( this.x < this.ninja.x - rightPoint) {
-            this.x = this.ninja.x - rightPoint;
-        } else if (this.x > this.ninja.x - leftPoint) {
-            this.x = this.ninja.x - leftPoint;
-        }
-        
-        // update verticle camera
-        let upperPoint = this.game.surfaceHeight / 2.5;
-        let lowerPoint = this.game.surfaceHeight - upperPoint;
-        if (this.y > this.ninja.y - upperPoint) {
-            this.y = this.ninja.y - upperPoint;
-        } else if (this.y < this.ninja.y - lowerPoint) {
-            this.y = this.ninja.y - lowerPoint;
-        }
-
-        if (this.title && this.game.click) {
-            if (this.game.click.y > 410 && this.game.click.y < 450) { // click start game
-                this.title = false;
-                this.loadLevel(this.level);
-            }
-        }
+    // update horizontal camera
+    let leftPoint = this.game.surfaceWidth / 3;
+    let rightPoint = this.game.surfaceWidth - leftPoint;
+    if (this.x < this.ninja.x - rightPoint) {
+      this.x = this.ninja.x - rightPoint;
+    } else if (this.x > this.ninja.x - leftPoint) {
+      this.x = this.ninja.x - leftPoint;
     }
 
-    loadLevel(level) {
-        this.clearEntities();
-        this.x = 0;
-        this.y = 0;
-
-        for (let i = 0; i < level.platforms.length; i++) {
-            let platform = level.platforms[i];
-            this.game.addEntity(new Platform(this.game, platform.x, platform.y, platform.width))
-        }
-
-        for (let i = 0; i < level.walls.length; i++) {
-            let wall = level.walls[i];
-            this.game.addEntity(new Wall(this.game, wall.x, wall.y, wall.height))
-        }
-
-        for (let i = 0; i < level.stumps.length; i++) {
-            let stump = level.stumps[i];
-            this.game.addEntity(new Stump(this.game, stump.x, stump.y))
-        }
-
-        for (let i = 0; i < level.grounds.length; i++) {
-            let ground = level.grounds[i];
-            console.log(ground);
-            this.game.addEntity(new Ground(this.game, ground.x, ground.y, ground.width))
-        }
-
-        for (let i = 0; i < level.slimes.length; i++) {
-            let slime = level.slimes[i];
-            this.game.addEntity(new Slime(this.game, slime.x, slime.y, slime.boss));
-        }
-
-        for (let i = 0; i < level.knights.length; i++) {
-            let knight = level.knights[i];
-            this.game.addEntity(new Knight(this.game, knight.x, knight.y))
-        }
-
-        for (let i = 0; i < level.ninjas.length; i++) {
-          let ninja = level.ninjas[i];
-          this.game.addEntity(new Ninja(this.game, ninja.x, ninja.y))
-        }
-
-        for (let i = 0; i < level.ghosts.length; i++) {
-          let ghost = level.ghosts[i];
-          this.game.addEntity(new Ghost(this.game, ghost.x, ghost.y))
-        }
-        this.ninja = new MainNinja(this.game, 0,170);
-        this.game.addEntity(this.ninja);
+    // update verticle camera
+    let upperPoint = this.game.surfaceHeight / 2.5;
+    let lowerPoint = this.game.surfaceHeight - upperPoint;
+    if (this.y > this.ninja.y - upperPoint) {
+      this.y = this.ninja.y - upperPoint;
+    } else if (this.y < this.ninja.y - lowerPoint) {
+      this.y = this.ninja.y - lowerPoint;
     }
 
-    clearEntities() {
-        this.game.entities.forEach(function (entity) {
-            entity.removeFromWorld = true;
-        });
-    };
-
-    draw(ctx) {
-        if (this.title) {
-            ctx.drawImage(ASSET_MANAGER.getAssset("sprites/BG.png"), 0, 0)
-
-            ctx.fillStyle = this.game.mouse
-            && this.game.mouse.y > 410 && this.game.mouse.y < 450 
-            ? "Grey": "White";
-            ctx.font = "50px serif"
-            ctx.fillText("Start Game", 385,450);
-        }
-
+    if (this.title && this.game.click) {
+      if (this.game.click.y > 410 && this.game.click.y < 450) {
+        // click start game
+        this.title = false;
+        this.loadLevel(this.level);
+      }
     }
 
+    if (this.ninja.hp <= 0 && !this.gameOver) {
+      this.gameOver = true;
+    }
+  }
+
+  loadLevel(level) {
+    this.clearEntities();
+    this.x = 0;
+    this.y = 0;
+
+    for (let i = 0; i < level.platforms.length; i++) {
+      let platform = level.platforms[i];
+      this.game.addEntity(
+        new Platform(this.game, platform.x, platform.y, platform.width)
+      );
+    }
+
+    for (let i = 0; i < level.walls.length; i++) {
+      let wall = level.walls[i];
+      this.game.addEntity(new Wall(this.game, wall.x, wall.y, wall.height));
+    }
+
+    for (let i = 0; i < level.stumps.length; i++) {
+      let stump = level.stumps[i];
+      this.game.addEntity(new Stump(this.game, stump.x, stump.y));
+    }
+
+    for (let i = 0; i < level.grounds.length; i++) {
+      let ground = level.grounds[i];
+      console.log(ground);
+      this.game.addEntity(
+        new Ground(this.game, ground.x, ground.y, ground.width)
+      );
+    }
+
+    for (let i = 0; i < level.slimes.length; i++) {
+      let slime = level.slimes[i];
+      this.game.addEntity(new Slime(this.game, slime.x, slime.y, slime.boss));
+    }
+
+    for (let i = 0; i < level.knights.length; i++) {
+      let knight = level.knights[i];
+      this.game.addEntity(new Knight(this.game, knight.x, knight.y));
+    }
+
+    for (let i = 0; i < level.ninjas.length; i++) {
+      let ninja = level.ninjas[i];
+      this.game.addEntity(new Ninja(this.game, ninja.x, ninja.y));
+    }
+
+    for (let i = 0; i < level.ghosts.length; i++) {
+      let ghost = level.ghosts[i];
+      this.game.addEntity(new Ghost(this.game, ghost.x, ghost.y));
+    }
+    this.ninja = new MainNinja(this.game, 0, 170);
+    this.game.addEntity(this.ninja);
+  }
+
+  clearEntities() {
+    this.game.entities.forEach(function (entity) {
+      entity.removeFromWorld = true;
+    });
+  }
+
+  draw(ctx) {
+    if (this.title) {
+      ctx.drawImage(ASSET_MANAGER.getAssset('sprites/BG.png'), 0, 0);
+
+      ctx.fillStyle =
+        this.game.mouse && this.game.mouse.y > 410 && this.game.mouse.y < 450
+          ? 'Grey'
+          : 'White';
+      ctx.font = '50px serif';
+      ctx.fillText('Start Game', 385, 450);
+    }
+
+    if (this.gameOver) {
+      ctx.filter = 'grayscale(1)';
+      ctx.fillStyle = 'red';
+      ctx.font = '50px serif';
+      ctx.fillText('GAME OVER', 385, 450);
+    }
+  }
 }
